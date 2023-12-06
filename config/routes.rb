35ded2ba8b4fee109523654d7c5d6e2ffd95f963
茅野恭平 <2111140015s@ed.fuk.kindai.ac.jp>
2023-12-06 07:24:03 +0000

    バリデーション & ユーザ一覧

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/index' => "user#index"
+  get "user/:id" => "user#show"
   get 'posts/index' => "posts#index"
+  get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   get 'home/top' => "home#top"
   get "home/about" => "home#about"
