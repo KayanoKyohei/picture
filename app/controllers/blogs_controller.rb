c36ffa61ddeb9e36e62d792cf4d061dabbe7d753
茅野恭平 <2111140015s@ed.fuk.kindai.ac.jp>
2023-12-20 08:37:23 +0000

    ブログの詳細　変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,6 +8,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @post = Blog.find_by(id:params[:id])
   end
 
   # GET /blogs/new
@@ -60,7 +61,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.find_by(id: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
